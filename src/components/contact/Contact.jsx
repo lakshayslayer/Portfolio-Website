982c74b26147388cbd4dad6d628d133c22496f00
Lakshay Garg <96138382+lakshayslayer@users.noreply.github.com>
2023-11-09 13:56:56 +0530

    spelling correction

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
-          <span>Ward No. 06, Ferozepur Jhirka, Haryan</span>
+          <span>Ward No. 06, Ferozepur Jhirka, Haryana</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Phone</h2>
